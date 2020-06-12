e9e3ee778ccd21d78c8e25a8da9f050fb8dc9934
Yanny Núñez <yannyesteban@hotmail.com>
2020-06-12 18:49:56 -0400

    ok

--- constantes.php
+++ constantes.php
@@ -1,23 +1,35 @@
 <?php
-define ("C_SERVIDOR","localhost");
-define ("C_USUARIO","root");
-define ("C_PASSWORD","");
-define ("C_BDATOS","cota");
+include("cbdatos.php");
 
-define ("C_MODULO_PRINCIPAL","sg_publico");
+define ("C_MODULO_PRINCIPAL","principal");
 define ("C_EST_DEFAULT","");
 define ("C_SG_USUARIO","user");
 define ("C_SG_CLAVE","123");
 
-define ("C_PATH","");
-define ("C_PATH_CONFIGURACION","");
-define ("C_PATH_QUERY","");
+define ("C_PATH","../sigefor2017/");
+define ("C_PATH_SEVIAN","../sevian/");
+define ("C_PATH_CONFIGURACION","../sigefor2017/");
+
+//define ("C_PATH","../sigefor21/");
+//define ("C_PATH_SEVIAN","../sevian/");
+//define ("C_PATH_CONFIGURACION","../sigefor2021/");
 define ("C_PATH_IMAGENES","imagenes/");
 define ("C_PATH_CSS","css/");
 define ("C_PATH_PLANTILLAS","plantillas/");
 define ("C_PATH_ARCHIVOS","archivos/");
 define ("C_PATH_GRAFICO","../jpgraph/");
+define ("C_PATH_REPORTES","");
 
+define ("C_HOJA_CSS","
+
+	css/seniat_2017.css,
+	css/calendario1.css,
+	../_sevian/sevian/css/sgMenu_.css,
+	../_sevian/sevian/css/sgWindow.css,
+	../_sevian/sevian/css/sgCalendar.css,
+	../_sevian/sevian/css/selectText.css,
+	
+");
 define ("C_PANEL_DEFAULT","4");
 define ("C_PANEL_DEBUG","8");
 
